c28bf87d0b6abcf727726ce7e9a65b1d1901e35d
Crowdbotics <team@crowdbotics.com>
2023-04-04 22:44:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vibosphere-39718",
+  url: "https://vibosphere-39718.botics.co",
+  api: "https://vibosphere-39718.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
